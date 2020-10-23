d279ae4845002bfb02822617428e1c4882ae81b6
Clamore <rotuman.lenoob@gmail.com>
2020-10-23 09:39:36 +0200

    Create entrerpise.php

---
+++ entrerpise.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<form method="post" >
+    <label>
+        Nom_entreprise : <input type="text" name="Nom de l'entreprise" />
+    </label>
+    <label>
+         : <input type="text" name="prenom" />
+    </label>
+
+    <input type="submit"/> <!--Juste le bouton pour submit ma requete -->
+
+
+
+
+
+</form>
+
+
+</body>
+</html>
