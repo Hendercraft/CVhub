2b2fd22d379624a072f570707c58d1d49c64c3f1
SavageBlink <31193280+SavageBlink@users.noreply.github.com>
2020-10-29 13:31:13 +0100

    Update signup.php with config.php
    Use of config.php

--- Site/signup.php
+++ Site/signup.php
@@ -1,19 +1,11 @@
 <?php
 
-ini_set('display_errors', 1); ini_set('log_errors',1); error_reporting(E_ALL); mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+ini_set('display_errors', 1);
+ini_set('log_errors',1);
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully <br>";
+require_once('config.php');
 
 
 if( $_POST["nom"] || $_POST["prenom"] ) {
