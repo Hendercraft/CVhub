7551f7f9361ec888813b0031938e75714434713d
SavageBlink <31193280+SavageBlink@users.noreply.github.com>
2020-10-31 13:47:35 +0100

    config

--- Site/config.php
+++ Site/config.php
@@ -8,14 +8,15 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 define('servername','localhost');
 define('username','root');
 define('password','');
+define('db','dbcv');
 
-$conn = new mysqli(servername, username, password);
+$conn = new mysqli(servername, username, password, db);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully<br>";
+//echo "Connected successfully<br>";
 
 set_error_handler(function($number,  $message) {
     echo "Handler captured error $number: '$message'" . PHP_EOL  ;
@@ -23,4 +24,11 @@ set_error_handler(function($number,  $message) {
 
 define('__ROOT__', dirname(__FILE__));
 
-
+if(strtoupper(substr(PHP_OS, 0, 3)) == 'WIN')
+{
+    define('LOGIN_P',__ROOT__.'\login.html');//win version
+    define('INDEX_P',__ROOT__.'\index.html');//win version
+} else {
+    define('LOGIN_P',__ROOT__.'/login.html'); //unix version
+    define('INDEX_P',__ROOT__.'/login.html'); //unix version
+}
