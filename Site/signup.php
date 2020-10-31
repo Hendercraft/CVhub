c647edd9cb8bf2e191d1a538063f87719cb35103
SavageBlink <31193280+SavageBlink@users.noreply.github.com>
2020-10-31 18:40:29 +0100

    signup.php une seule inscription possible
    Il n'est maintenant plus possible de s'inscrire plusieurs fois.

--- Site/signup.php
+++ Site/signup.php
@@ -10,10 +10,9 @@ require_once('config.php');
 
 if( $_POST["nom"] || $_POST["prenom"] ) {
     if (preg_match("/[^A-Za-z'-]/",$_POST['nom'] ) && preg_match("/[^A-Za-z'-]/",$_POST['prenom'] )) {
-        die ("invalid name and name should be alpha");
+        die ("invalid name , name should be only composed of letters");
     }
-    echo "Welcome ". $_POST['nom']. "<br />";
-    echo "You are ". $_POST['prenom']. " and i am dumb";
+
 }
 $email = $_POST['email'];
 $firstname = $_POST['prenom'];
@@ -25,14 +24,52 @@ $mdp = password_hash($_POST['mdp'],PASSWORD_DEFAULT );
 $born_date = html_entity_decode($_POST['born_date']);
 $fborn_date = date('Y-m-d',strtotime($born_date));
 
-$sql = "INSERT INTO dbcv.users(nom,prenom,adresse,date_naissance,email,password,telephone,linkdin)
-VALUES ('$nom','$firstname','$adresse','$fborn_date','$email','$mdp',$tel,'$lnk')";
+/*$sql = "INSERT INTO dbcv.users(nom,prenom,adresse,date_naissance,email,password,telephone,linkdin)
+VALUES ('$nom','$firstname','$adresse','$fborn_date','$email','$mdp',$tel,'$lnk')";*/
 
-if ($conn->query($sql) == TRUE) {
-    echo "<br>New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+$signup = 'INSERT INTO dbcv.users(nom,prenom,adresse,date_naissance,email,password,telephone,linkdin)
+VALUES (?,?,?,?,?,?,?,?)';
+
+$verify = 'SELECT * from `users` WHERE `users`.email LIKE ?';
+
+
+if($ver = $conn->prepare($verify))
+{
+
+
+    $ver->bind_param('s',$email);
+
+
+    $ver->execute();
+
+    $ver->store_result();
+
+    if($ver->num_rows !== 0)
+    {
+        //echo $ver->num_rows;
+        echo '<h3 style="color:red;">Email déjà utilisé</h3><br>';
+        require_once(SIGNUP_P);
+    }
+    else
+    {
+        echo $ver->num_rows;
+        $ver->close();
+        if ($stmt = $conn->prepare($signup)) {
+            $stmt->bind_param('ssssssds',$nom,$firstname,$adresse,$fborn_date,$email,$mdp,$tel,$lnk);
+
+            $stmt->execute();
+
+            //echo "<br>New record created successfully";
+            $stmt->close();
+            require_once(INDEX_P);
+        } else {
+            echo "Error: " . $signup . "<br>" . $conn->error;
+        }
+    }
 }
+
+
+
 
 $conn->close();
 
@@ -43,7 +80,7 @@ set_error_handler(function($number,  $message) {
 });
 
 
-require_once(INDEX_P);
+
 
 //header('http://'.__ROOT__.'\index.html');
 //phpinfo();
