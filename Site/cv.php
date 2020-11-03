6b43cc1a1b5ce1db73391aec088361475a322130
Clamore <rotuman.lenoob@gmail.com>
2020-11-03 20:31:23 +0100

    moved all

--- Site/cv.php
+++ Site/cv.php
@@ -1,7 +1,6 @@
 <?php 
 header("Content-type: text/css");
 $primar=$_POST["primar"];
-
 ?>
 @import url('https://themes.googleusercontent.com/fonts/css?kit=Yeh3y7ciGQhij8XzUSI-wN9BwJ9QSusUFFTiN987YIbvi2WQbTlLajdXq3UgoDiw');
             ol{
