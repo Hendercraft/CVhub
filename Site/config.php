21893e7878de576e42f413ff987f68bc755227e6
driss <drisskihal@gmail.com>
2020-11-08 07:55:24 +0100

    CV are ugly but working

--- Site/config.php
+++ Site/config.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('log_errors',1);
 error_reporting(E_ALL);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);*/
 
 
 define('servername','localhost');
@@ -18,9 +18,9 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully<br>";
 
-set_error_handler(function($number,  $message) {
+/*set_error_handler(function($number,  $message) {
     echo "Handler captured error $number: '$message'" . PHP_EOL  ;
-});
+});*/
 
 define('__ROOT__', dirname(__FILE__));
 
